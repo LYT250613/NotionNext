c518fead27206e1fea4809db4f79537e4c29c595
LYT250613 <lixiankun001@163.com>
2025-08-25 10:52:13 +0800

    Update Footer.js

--- themes/typography/components/Footer.js
+++ themes/typography/components/Footer.js
@@ -27,17 +27,17 @@ export default function Footer(props) {
     <footer className="w-full">
       <DarkModeButton className='pt-4' />
       
-      {/* 青梧新论十年之约链接 */}
-      <div className="flex justify-center my-4">
+      {/* 青梧新论十年之约链接 - 进一步调整图片大小 */}
+      <div className="flex justify-center my-2">
         <a 
           href="https://www.snzy.cn/website/2025081312000063.html" 
           title="青梧新论的十年之约" 
           target="_blank" 
           rel="noopener noreferrer"
-          className="transition-transform hover:scale-105"
+          className="transition-transform hover:scale-105 inline-flex opacity-80 hover:opacity-100"
         >
           <img 
-            style={{width: '100px', height: '36px'}} 
+            className="h-5 w-auto" 
             src="https://www.snzy.cn/images/snzylogo.png" 
             alt="青梧新论十年之约"
           />
@@ -45,7 +45,7 @@ export default function Footer(props) {
       </div>
       
       {/* 版权信息 */}
-      <div className='font-bold text-[var(--primary-color)] dark:text-white py-6 text-sm flex flex-col gap-2 items-center'>
+      <div className='font-bold text-[var(--primary-color)] dark:text-white py-3 text-sm flex flex-col gap-1 items-center'>
         <div>
           &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}.
         </div>
